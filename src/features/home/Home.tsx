96cbc2ca318c7108372cb8121ba48a6a9195d5db
Ian Tavares <iantavares2405@gmail.com>
2023-09-09 23:30:51 -0300

    (refactor):  update Home

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,42 +1,10 @@
-import { useEffect, useState } from 'react'
-
-import { Page, MovieCard } from '../../components'
-import { Movie } from '../../types/Movie'
-import { getImage, getPopularMovies } from '../../services/api'
-import { Box } from '@mui/material'
+import { Page, MoviesRow } from '../../components'
 
 export const Home = () => {
-  const [movies, setMovies] = useState<Movie[]>([])
-
-  useEffect(() => {
-    const getMovies = async () => {
-      const response = await getPopularMovies()
-      setMovies(response.results)
-    }
-
-    getMovies()
-  }, [])
-
   return (
     <Page hasTopBar>
-      <Box display={'flex'} gap={8}>
-        {movies.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            title={movie.title}
-            imageUrl={getImage(movie.poster_path)}
-          />
-        ))}
-      </Box>
-      <Box display={'flex'} gap={8}>
-        {movies.map((movie) => (
-          <MovieCard
-            key={movie.id}
-            title={movie.title}
-            imageUrl={getImage(movie.poster_path)}
-          />
-        ))}
-      </Box>
+      <MoviesRow list="now_playing" />
+      <MoviesRow list="popular" />
     </Page>
   )
 }
