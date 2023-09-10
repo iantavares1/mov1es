0809d6d60528d28a2d98f663714052552b39779b
Ian Tavares <iantavares2405@gmail.com>
2023-09-09 23:18:17 -0300

    (refactor): add list param at getMovies function

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,17 +1,9 @@
 import axios from 'axios'
+import { MovieList } from '../types/MovieList'
 
 const apiReadToken = import.meta.env.VITE_API_READ_TOKEN
 
-export const getImage = (posterPath: string) =>
-  `https://image.tmdb.org/t/p/w500/${posterPath}`
-
-export const searchMovie = async (movieId: number) =>
-  callAPI(`https://api.themoviedb.org/3/movie/${movieId}`)
-
-export const getPopularMovies = async (page?: number) =>
-  callAPI('https://api.themoviedb.org/3/movie/upcoming', { page })
-
-export const callAPI = async (url: string, additionalParams?: object) => {
+const callAPI = async (url: string, additionalParams?: object) => {
   const options = {
     method: 'GET',
     params: {
@@ -32,3 +24,12 @@ export const callAPI = async (url: string, additionalParams?: object) => {
     return error
   }
 }
+
+export const getImage = (posterPath: string) =>
+  `https://image.tmdb.org/t/p/w500/${posterPath}`
+
+export const getMovies = async (list: MovieList = 'popular', page?: number) =>
+  callAPI(`https://api.themoviedb.org/3/movie/${list}`, { page })
+
+export const searchMovie = async (movieId: number) =>
+  callAPI(`https://api.themoviedb.org/3/movie/${movieId}`)
