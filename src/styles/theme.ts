6bcd206c19327e0f97cbe69900535510a6f9616b
Ian Tavares <iantavares2405@gmail.com>
2023-09-10 22:08:18 -0300

    feat: add size functions to theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,17 @@
 import { createTheme } from '@mui/material'
 
+const REM_SIZE = 16
+const REM_OFFSET = 0.6
+const PX_OFFSET = 0.4
+interface CustomTheme {
+  size: (size: number, unit?: 'rem' | 'px') => string
+  fontSize: (size: number, unit?: 'rem' | 'px') => string
+}
+declare module '@mui/material/styles' {
+  interface Theme extends CustomTheme {}
+  interface ThemeOptions extends CustomTheme {}
+}
+
 export const theme = createTheme({
   palette: {
     text: {
@@ -13,4 +25,7 @@ export const theme = createTheme({
   typography: {
     fontFamily: 'Inter',
   },
+  size: (size, unit = 'rem') =>
+    `calc(${(size / REM_SIZE) * REM_OFFSET}${unit} + ${size * PX_OFFSET}px)`,
+  fontSize: (size, unit = 'rem') => `${size / REM_SIZE}${unit}`,
 })
