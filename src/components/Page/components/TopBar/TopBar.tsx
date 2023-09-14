08ebf2325e99a7a55132771076a48c1624fb6e36
Ian Tavares <iantavares2405@gmail.com>
2023-09-14 00:10:16 -0300

    refactor(TopBar): remove styles archive

--- src/components/Page/components/TopBar/TopBar.tsx
+++ src/components/Page/components/TopBar/TopBar.tsx
@@ -1,15 +1,28 @@
 import { useSearch } from '../../contexts/SearchContext'
-import { TopBarStyled } from './TopBar.styles'
+import { Box, styled } from '@mui/material'
 import { Search } from './Search/Search'
 import { Logo } from '@/assets/Logo'
 
 export const TOPBAR_HEIGHT = '70px'
 
+const TopBarStyled = styled(Box)(({ theme }) => ({
+  position: 'fixed',
+  top: 0,
+  left: 0,
+  zIndex: 3,
+  height: TOPBAR_HEIGHT,
+  width: '100vw',
+  padding: `0 ${theme.size(100)}`,
+  background: theme.palette.background.default,
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+}))
+
 export const TopBar = () => {
   const { searchValue, handleSearch } = useSearch()
-
   return (
-    <TopBarStyled px={12}>
+    <TopBarStyled>
       <Logo hasText />
       <Search value={searchValue} onChange={handleSearch} />
     </TopBarStyled>
