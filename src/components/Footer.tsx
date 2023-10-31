1c68fd2e265f42166105919fbd0aaf42e1dedde4
Ian Tavares <iantavares2405@gmail.com>
2023-10-30 22:55:12 -0300

    init remake

---
+++ src/components/Footer.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export const Footer = () => {
+  return (
+    <footer className="flex justify-between">
+      <Link href={"/"} className="flex gap-2">
+        <Image width={28} height={28} src="logo.svg" alt="logo" />
+      </Link>
+
+      <div className="flex items-center gap-2">
+        <span className="hidden text-sm sm:block ">
+          Este produto usa a API TMDB, mas não é endossado ou certificado pelo
+          TMDB
+        </span>
+
+        <Link href={"https://www.themoviedb.org/"} target="blank">
+          <Image width={48} height={48} src="tmdb-logo.svg" alt="TMDB logo" />
+        </Link>
+      </div>
+    </footer>
+  )
+}
