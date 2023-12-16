575968976d04d23dd4b10ae24d2b65f049edf117
Ian Tavares <iantavares2405@gmail.com>
2023-12-15 22:12:02 -0300

    feat: add Movie type

---
+++ src/types/Movie.ts
@@ -0,0 +1,10 @@
+export type Movie = {
+  adult: boolean
+  id: number
+  original_title: string
+  poster_path: string
+  backdrop_path: string
+  vote_average: number
+  title: string
+  name: string
+}
