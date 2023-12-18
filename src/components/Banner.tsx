22eb0d5dfa306979e610b94dac4bb63038c41923
Ian Tavares <iantavares2405@gmail.com>
2023-12-18 20:00:03 -0300

    refactor(Banner): use TMDB_IMAGE path to get source

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,14 +1,15 @@
-import { Movie } from "@/types/Movie"
 import Image from "next/image"
+import { TMDB_IMAGE } from "@/services/paths"
+import { Movie } from "@/types"
 
 export function Banner({ poster }: { poster: Movie }) {
   return (
     <div className="mb-3">
       <Image
         alt={`poster de  ${poster.title}`}
-        src={`https://image.tmdb.org/t/p/original${poster.backdrop_path}`}
+        src={TMDB_IMAGE(poster.backdrop_path)}
         placeholder="blur"
-        blurDataURL={`https://image.tmdb.org/t/p/original${poster.backdrop_path}`}
+        blurDataURL={TMDB_IMAGE(poster.backdrop_path)}
         priority
         width={0}
         height={0}
