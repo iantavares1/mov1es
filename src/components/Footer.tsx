7f9a4279af9444401ab35e7c057e4f37d4c082b0
Ian Tavares <iantavares2405@gmail.com>
2023-12-21 14:07:23 -0300

    feat: add back top button and position fixed to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,44 @@
+"use client"
+
+import { useEffect, useState } from "react"
 import Image from "next/image"
 import Link from "next/link"
 import tmdbLogo from "../../public/tmdb-logo.svg"
+import { ArrowCircleUp } from "@mui/icons-material"
 
 export const Footer = () => {
+  const [showBackTopButton, setShowBackTopButton] = useState(false)
+
+  const handleScroll = () =>
+    setShowBackTopButton(
+      document.body.scrollTop > 20 || document.documentElement.scrollTop > 20
+        ? true
+        : false
+    )
+
+  const backToTop = () => {
+    document.body.scrollTop = 0
+    document.documentElement.scrollTop = 0
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll)
+
+    return () => window.removeEventListener("scroll", handleScroll)
+  }, [])
+
   return (
-    <footer className="flex h-[10dvh] w-screen justify-end bg-primary p-300">
+    <footer
+      className={`fixed bottom-0 z-50 flex h-[8dvh] w-screen ${
+        showBackTopButton ? "justify-between" : "justify-end"
+      } bg-primary px-300`}
+    >
+      {showBackTopButton && (
+        <button onClick={backToTop}>
+          <ArrowCircleUp />
+        </button>
+      )}
+
       <div className="flex items-center gap-2">
         <span className="hidden text-sm sm:block">
           Este produto usa a API TMDB, mas não é endossado ou certificado pelo
