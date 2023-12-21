e6927f21ea1451feb7ddf5e0aabedfa9a5bae896
Ian Tavares <iantavares2405@gmail.com>
2023-12-21 00:28:12 -0300

    refactor: update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,19 +8,32 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundColor: {
+      colors: {
         primary: "#001524",
-        secondary: "#001d3d",
-        tertiary: "#003566",
-        light: "#FFC300",
-        ultraLight: "#FFD60A",
-      },
-      textColor: {
         onPrimary: "#FFFFFF",
+        secondary: "#001d3d",
         onSecondary: "#FFFFFF",
+        tertiary: "#003566",
         onTertiary: "#FFFFFF",
+        light: "#FFC300",
         onLight: "#000000",
+        ultraLight: "#FFD60A",
         onUltraLight: "#000000",
+      },
+      spacing: {
+        100: "5px",
+        200: "10px",
+        300: "15px",
+        400: "20px",
+        500: "25px",
+        600: "30px",
+        700: "35px",
+        800: "40px",
+        900: "45px",
+        1000: "50px",
+      },
+      gridTemplateColumns: {
+        100: "repeat(auto-fill, minmax(95px, 1fr))",
       },
     },
   },
